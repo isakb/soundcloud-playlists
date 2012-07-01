cb51adb57fef93e01da74fd5acd9e13658cfe3f3
Isak Bakken <isak@klarna.com>
2012-07-01 19:26:34 +0200

    Allow localhost client

--- src/config/sc_app_config.js
+++ src/config/sc_app_config.js
@@ -3,7 +3,11 @@
  */
 define({
     // The id of my soundcloud app:
-	client_id: '65ffd37ad466cc917f34ba2767b2458a',
+    client_id:( location.hostname === 'localhost' ?
+                // Development mode app.
+                'a0d8ca659b47f50a1873b2e58cf12edd'
+                // Production mode app.
+                : '65ffd37ad466cc917f34ba2767b2458a'),
 
     // If we want to be able to play ALL tracks (even non-public), we actually
     // need to auth first.
