b38ba904a06acf894de78c5dcc9b4c998cbf584d
Isak Bakken <isak@klarna.com>
2012-07-01 04:49:16 +0200

    Allow connect / auth

--- src/config/sc_app_config.js
+++ src/config/sc_app_config.js
@@ -3,5 +3,9 @@
  */
 define({
     // The id of my soundcloud app:
-	client_id: '65ffd37ad466cc917f34ba2767b2458a'
+	client_id: '65ffd37ad466cc917f34ba2767b2458a',
+
+    // If we want to be able to play ALL tracks (even non-public), we actually
+    // need to auth first.
+    redirect_uri: ('' + location).replace(/index.html.*/, 'callback.html')
 });
